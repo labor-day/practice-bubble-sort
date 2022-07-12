ddcd90a9ee1c8997eb1869e5c1a5b9df3bdcf971
labor-day <shaffer.scott@yahoo.com>
2022-07-12 16:17:19 -0600

    implemented bubble sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,21 @@
 
 function bubbleSort(arr) {
 
+  let swaps = 0;
+
+  do {
     // Iterate through the array
-
-      // If the current value is greater than its neighbor to the right
-        // Swap those values
-
-        // Do not move this console.log
+    swaps = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i+1]) {
+        [arr[i+1], arr[i]] = [arr[i], arr[i+1]];
+        swaps++;
         console.log(arr.join(","));
+      }
+    }
+  } while (swaps > 0);
 
-    // If you get to the end of the array and no swaps have occurred, return
-
-    // Otherwise, repeat from the beginning
-
+  return arr;
 }
 
 module.exports = bubbleSort;
